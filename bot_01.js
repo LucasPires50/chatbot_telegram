398d6491b166df5d158096e8034a0f24419b641e
Lucas Pires <luc_50@outlook.com>
2020-10-21 20:24:57 -0300

    1º bot que reponde texto

---
+++ bot_01.js
@@ -0,0 +1,15 @@
+const env = require('./.env')
+const telegraf =  require('telegraf')
+const bot = new telegraf(env.token)
+
+bot.use(async(ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date - start
+    const dataEHora = new Date().toLocaleString();
+    console.log(`${dataEHora}\n Tempo de resposta: ${ms}ms`)
+})
+
+bot.on('text', ctx => ctx.reply('Alo Mundo SENAI'))
+
+bot.launch()
