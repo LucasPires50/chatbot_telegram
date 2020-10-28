4e22a2131ce3a59c1535aa3acb2fe4762703af92
Lucas Pires <luc_50@outlook.com>
2020-10-28 20:56:05 -0300

    arrumando erro

--- bot_04.js
+++ bot_04.js
@@ -1,65 +1,65 @@
-const env = require('./.env')
-const telegraf =  require('telegraf')
-const bot = new telegraf(env.token)
+const env = require('./.env');
+const telegraf = require('telegraf');
 
-//escutado o comando /start
-bot.start(ctx => {
-    const nome = ctx.message.from.first_name + ' ' + ctx.message.from.last_name
-    ctx.reply(`Seja bem vindo ${nome}!`)
+const bot = new telegraf(env.token);
+
+//executando o star
+bot.start( ctx =>{
+    const nome = ctx.message.from.first_name + '-'+ ctx.message.from.last_name
+    ctx.reply(`Seja bem vindo ${nome}`)
+    ;
 })
 
-// evento de texto
-bot.on('text,', ctx => {
-    ctx.reply(`Texto: ${ctx.message.text} recebido com sucesso`)
-})
+//evento de texto
+bot.on('text',ctx => {
+    ctx.reply(`text:${ctx.message.text} recebido com sucesso.`)
+});
 
-//evento de localização
-bot.on('location', ctx => {
+// evento de localização 
+bot.on('location',ctx =>{
     const location = ctx.message.location
-    ctx.reply(`Sei que você está na latitude: ${location.latitude} e longitude: ${location.longitude}`)
+    ctx.reply(`Sei que você esta na latitude ${location.latitude} e logitude ${
+        location.longitude}`)
 })
-
-//evento de contato
-bot.on('contact', ctx => {
-    const contact = ctx.message.contact
-    console.log(contact)
-    ctx.reply(`Vou guardar o contato de ${contact.first_name} e telefone ${contact.phone_number}`)
+//evento de contato 
+bot.on('contact', ctx =>{
+    const contact = ctx.message.contact;
+    console.log(contact);
+    ctx.reply(`Vou guardar o contato de ${contact.first_name} e telefone ${contact.phone_number}`);
 })
-
-//evento de voz
-bot.on('voice', ctx => {
-    const voice = ctx.message.voice
-    console.log(voice)
-    ctx.reply(`Áudio recebido, ele possui ${voice.duration}s`)
-
+//Evento de Voz
+bot.on('voice',ctx =>{
+    const voice = ctx.message.voice;
+    console.log(voice);
 })
-
 //evento de foto
-bot.on('photo', ctx => {
+bot.on('photo', async ctx =>{
+
     const photo = ctx.message.photo
+    const legend = ctx.message.caption
     console.log(photo)
-    photo.forEach((ft, i) => {
-        ctx.reply(`Foto ${i} tem rosolução de ${ft.width}x${ft.height}`)
-    });
-//arrumar
-    for([i, ft] of photo.entries()){
-        await ctx.reply(`Foto ${i} tem`)
+
+    for([i,ft] of photo.entries()){    
+      await ctx.reply(`foto ${i} tem resolução ${ft.width} x ${ft.height}`)
     }
-})
 
-// evento de figurinhas
-bot.on('sticker', ctx => {
-    const sticker = ctx.message.sticker
-    console.log(sticker)
-    ctx.reply(`Você enviou uma figurinha correspondente ${sticker.emoji} do pacote ${sticker.set_name}`)
+    ctx.reply(`legenda: ${legend}`)
 })
+//evento figurinha
+bot.on('stricker',ctx =>{
 
-//evento de animação
-bot.on('animation', ctx => {
-    const animation = ctx.message.animation
-    console.log(animation)
-    ctx.reply(`Esta animação dura ${animation.duration}s e o tamanho de arquivo é ${animation.file_size} byte`)
+    const figurinha = ctx.message.stricker
+    console.log(figurinha)    
+    ctx.reply(`voce envio uma figurinha ${figurinha.emoji} do pacote ${figurinha.set_name}`)  
+    
+    
 })
+//animação gif animado
+bot.on('animation',ctx =>{
 
-//para o bot funbcionar
-bot.launch()
+    const animacao = ctx.message.animation
+    console.log(animacao)    
+    ctx.reply(`animação ${animation.duraction} do pacote ${animation.file_size} bytes`)    
+
+})
+bot.launch();
--- bot_05.js
+++ bot_05.js
@@ -1,40 +1,32 @@
-const env = require('./.env')
-const telegraf =  require('telegraf')
-const bot = new telegraf(env.token)
+const env = require('./.env');
+const telegraf = require('telegraf');
 
-const gitTelegram = 'http://techcrunch.com/2014/02/24/telegram-saw-8m-downloads-after-whatsapp-got-acquired/'/
+const bot = new telegraf(env.token);
+const giftTelegram = 'https://media.giphy.com/media/ya4eevXU490Iw/giphy.gif';
 
-bot.use(async(ctx, next) => {
-    const start = new Date()
-    await next()
-    const ms = new Date - start
-    const dataEHora = new Date().toLocaleString();
-    console.log(`${dataEHora}\n Tempo de resposta: ${ms}ms`)
-})
 
-// diferentes tipos de respostas
+
+//diferentes tipos de respostas 
 bot.start(async ctx => {
-    //Resposta texto
-    await ctx.reply(`Seja Bem Vindo, ${ctx.message.from.first_name}!:+1:`)
+    await ctx.reply(`Seja Bem vindo,${ctx.message.from.first_name}!`)
 
-    await ctx.reply(`Veja o video: https://youtu.be/oIM5hljQQaA`)
+    //resposta com video
+    await ctx.reply('Veja o video: https://www.youtube.com/watch?v=QeUoxscV8FQ  ${ctx.message.from.fist_name}!')
 
-    // await ctx.replyWithHTML (`Pode ser usado tags <strong>Para negrito</strong ou para <em>Itálico</em>. <a href="https://sc.senai.br">Senai</a> <code>Código</code>`)
+    // resposta em HTMl
+    await ctx.replyWithHTML(`pode ser usar tags html <strong>bem vindo</strong>`)
 
-    // //resposta com Markdown
-    // await ctx.replyWithMarkdown('Dá para escrever "em negrito", _em itálico_ `em código` ou ```bloco de código```, também é possivel Link [SENAI](https://sc.senai.br)')
+    // resposta em Markdown
+    await ctx.replyWithMarkdown('dá para escrever *em negrito*, _em italico_ `em codifo` ou ```bloco de codigo```.tambem é possivel  link [Senai](https://sc.senai.br)')
 
-    //Resposta foto
-    await ctx.replyWithPhoto('https://picsum.photos/200/300/?random', {caption: 'Foto Aleátoria'})
-
-    //resposta com localização
-    await ctx.replyWithLocation(-27.1165927, -48.9123907)
-
-    //resposta com gif
-    await ctx.replyWithAnimation(gitTelegram)
-
+    //resposta em foto
+    await ctx.replyWithPhoto('https://picsum.photos/200/300/?random',{caption: 'foto aleatoria'})
+    
+    //resposta com localizção
+    await ctx.replyWithLocation(-27.1156927,-48.9123907)
+    
+    // resposta com gif animado
+    await ctx.replyWithAnimation(giftTelegram)
 })
 
-
-
-bot.launch()
+bot.launch();
